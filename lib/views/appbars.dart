87205b1c235bc06a5eef19ed21c3f19b20ee6f4f
1328148451@qq.com <1328148451@qq.com>
2022-01-13 13:15:39 +0800

    login appbar2

--- lib/views/appbars.dart
+++ lib/views/appbars.dart
@@ -13,45 +13,55 @@ class BeeAppBar extends ZkGetfindView<FlyBeeFilter>
     return AppBar(
         title: Text(controller.labelTextOf(zkValueKey) ?? "appbar"),
         centerTitle: true,
-        leading: (zkValueKey!.value == 'home_appbar')
-            ? Column(
-                children: [
-                  SizedBox(
-                    width: 40,
-                    height: 40,
-                    child: ZkIconButton(
-                      key: FlybeeKey.beeKeyBtnMonitor,
-                      filter: controller,
-                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 2),
-                      icon: const ClipOval(
-                        child: Image(
-                            image: AssetImage('assets/images/monitor_noc.jpg')),
-                      ),
-                    ),
+        leading: _buildLeading(),
+        actions: _buildAction(context));
+  }
+
+// 设置_模式
+  Widget? _buildLeading() {
+    return (zkValueKey!.value == 'home_appbar')
+        ? Column(
+            children: [
+              SizedBox(
+                width: 40,
+                height: 40,
+                child: ZkIconButton(
+                  key: FlybeeKey.beeKeyBtnMonitor,
+                  filter: controller,
+                  padding: const EdgeInsets.fromLTRB(0, 10, 0, 2),
+                  icon: const ClipOval(
+                    child: Image(
+                        image: AssetImage('assets/images/monitor_noc.jpg')),
                   ),
-                  const Text('监控模式', style: TextStyle(fontSize: 10)),
-                ],
-              )
-            : ZkIconButton(
-                key: FlybeeKey.beeKeyBtnBack,
-                filter: controller,
-                icon: controller.prefixIconOf(FlybeeKey.beeKeyBtnBack)!,
+                ),
               ),
-        actions: [
-          IconButton(
-            icon: Column(children: const [
-              Icon(
-                Icons.person,
-                color: Colors.white,
-              ),
-              Text('admin', style: TextStyle(fontSize: 10))
-            ]),
-            iconSize: 24,
-            splashRadius: 20,
-            onPressed: () {
-              Scaffold.of(context).openEndDrawer();
-            },
+              const Text('监控模式', style: TextStyle(fontSize: 10)),
+            ],
+          )
+        : ZkIconButton(
+            key: FlybeeKey.beeKeyBtnBack,
+            filter: controller,
+            icon: controller.prefixIconOf(FlybeeKey.beeKeyBtnBack)!,
+          );
+  }
+
+// action
+  List<Widget> _buildAction(context) {
+    return [
+      IconButton(
+        icon: Column(children: const [
+          Icon(
+            Icons.person,
+            color: Colors.white,
           ),
-        ]);
+          Text('admin', style: TextStyle(fontSize: 10))
+        ]),
+        iconSize: 24,
+        splashRadius: 20,
+        onPressed: () {
+          Scaffold.of(context).openEndDrawer();
+        },
+      ),
+    ];
   }
 }
